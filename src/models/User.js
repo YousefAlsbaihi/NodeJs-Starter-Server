a9e6d79adf5a07f5a6f1cfdc364daa4c6c3e9830
Joe <You.Alsbaihi@gmail.com>
2024-05-24 12:02:49 -0700

    Update User.js
    Trim and lowecase the email

--- src/models/User.js
+++ src/models/User.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: false },
+    name: { type: String, required: true, unique: false, trim: true },
     password: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true, lowercase: true, trim: true },
     profile_picture: { type: String, required: false, unique: false },
     active: { type: Boolean, default: true },
     permissions: [{
